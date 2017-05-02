0e52715cad4e94f9ffafcc2128ea4d0f8703d3d1
Changbeom Choi <cbchoi@handong.edu>
2017-05-02 12:48:52 +0900

    libLucidPID
    Modularized makefile
    Visualization tool added

---
+++ visualize/graph.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+P_data = []
+PD_data = []
+PI_data = []
+PID_data = []
+
+fp = open("../bin/test_P.dat")
+for line in fp:
+	P_data.append(line)
+fp.close()
+
+fp = open("../bin/test_PI.dat")
+for line in fp:
+	PI_data.append(line)
+fp.close()
+
+fp = open("../bin/test_PD.dat")
+for line in fp:
+	PD_data.append(line)
+fp.close()
+
+fp = open("../bin/test_PID.dat")
+for line in fp:
+	PID_data.append(line)
+fp.close()
+
+
+plt.figure(1)
+plt.subplot(411)
+plt.plot(P_data)
+plt.ylabel('P Controller')
+
+plt.subplot(412)
+plt.plot(PI_data)
+plt.ylabel('PI Controller')
+
+plt.subplot(413)
+plt.plot(PD_data)
+plt.ylabel('PD Controller')
+
+plt.subplot(414)
+plt.plot(PID_data)
+plt.ylabel('PID Controller')
+
+plt.show()
